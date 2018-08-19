3eadfe36c7ebee2da08b23980151e1dfceefcc8a
Azez Nassar <azez.nassar@yahoo.com>
2018-08-19 19:31:58 +1200

    import app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+    render(){
+        return (
+            <div className="app">
+                <header className="appHeader">
+                    <h1>React-Parcel boilerplate</h1>
+                </header>
+                <div className="content">
+                    <h3>Modify <code>src/App.js</code> and save</h3>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './app';
 import './stylesheet.css';
-
-class App extends React.Component {
-    render(){
-        return (
-            <div className="app">
-                <header className="appHeader">
-                    <h1>React-Parcel boilerplate</h1>
-                </header>
-                <div className="content">
-                    <h3>Modify <code>src/index.js</code> and save</h3>
-                </div>
-            </div>
-        );
-    }
-}
 
 ReactDOM.render(<App/>, document.getElementById('root'));
