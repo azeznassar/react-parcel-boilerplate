ee27651285787ddb1cacb3296268502efc936ce4
Azez Nassar <azez.nassar@yahoo.com>
2018-11-05 19:01:58 +1300

    Updated to React Hooks

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
 # React-Parcel boilerplate
 
-React boilerplate/starter pack using Parceljs
- 
+React boilerplate using Parceljs as the bundler.
+
 ### Version
 
-1.0.0
-
-## Usage
-
-Boilerplate for React using Parceljs
+1.0.1
 
 ### Installation
 Install the dependencies:
@@ -22,7 +18,7 @@ $ npm install
 
 ### Run
 
-This will watch your files, buddle them and run your local dev server at http://localhost:1234/
+Will watch the files, buddle them and run on local dev server at http://localhost:1234/:
 
 ```sh
 $ npm start
--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends React.Component {
-    render(){
-        return (
-            <div className="app">
-                <header className="appHeader">
-                    <h1>React-Parcel boilerplate</h1>
-                </header>
-                <div className="content">
-                    <h3>Modify <code>src/App.js</code> and save</h3>
+function App() {
+
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="app">
+            <header className="appHeader">
+                <h1>React-Parcel boilerplate</h1>
+            </header>
+            <div className="content">
+                <h3>Modify <code>src/App.js</code> and save</h3>
+                <div>
+                    <button onClick={() => setCount(count + 1)}>Click me</button>
+                    <p>You clicked {count} times</p>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default App;
